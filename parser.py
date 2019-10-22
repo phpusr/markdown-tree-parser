2bfa5a1088bae2179fcca282e707345794a948d0
phpusr <phpusr@gmail.com>
2019-10-22 19:25:07 +0500

    Fixes

--- parser.py
+++ parser.py
@@ -2,25 +2,37 @@ import re
 
 
 class Element:
-    _source = ''
-    children = []
+    def __init__(self):
+        self._source = ''
+        self.children = []
 
     def add_child(self, el):
         self.children.append(el)
 
     def add_source(self, source):
-        self._source += source + '\n'
+        self._source += source
 
     def __getitem__(self, item):
         return self.children[item]
 
+    @property
+    def source(self):
+        return self._source
+
 
 class Out(Element):
-    title = None
+    root = None
+
+    @property
+    def title(self):
+        if self.root is not None:
+            return self.root.text
 
 
 class Heading(Element):
-    def __init__(self, level, text):
+    def __init__(self, parent, level, text):
+        super().__init__()
+        self.parent = parent
         self.level = level
         self.text = text
 
@@ -31,39 +43,52 @@ class Heading(Element):
 class Parser:
     def parse(self, text):
         self.out = Out()
-        self.current = self.out
+        self.current = None
 
-        for string in text.split('\n'):
+        strings = text.split('\n')
+        for index in range(len(strings)):
+            string = strings[index]
             is_heading = False
             for level in range(6):
-                is_heading = self.parse_heading(level + 1, string)
+                is_heading = self._parse_heading(level + 1, string)
                 if is_heading:
                     break
+                # TODO parse heading with '---' and '==='
 
             if not is_heading:
-                self.current.add_source(string)
+                ending = '' if index == len(strings) - 1 else '\n'
+                if self.current is None:
+                    self.out.add_source(string + ending)
+                else:
+                    self.current.add_source(string + ending)
 
         return self.out
 
-    def parse_heading(self, level, string):
+    def _parse_heading(self, level, string):
         if False:
             print(f' - parse heading with level: {level}, string: {string}')
 
         regex = '^\s?#{%s}\s+(.*)$' % level
         result = re.search(regex, string)
+
         if result is None:
             return False
 
         text = result[1]
 
-        if level == 1 and self.out.title is None:
-            self.out.title = text
-            return True
+        if self.current is None:
+            parent = self.out
+        elif level > self.current.level:
+            parent = self.current
+        else:
+            parent = self.current.parent
 
-        # TODO
-        self.parent = self.current
-        self.current = Heading(level, text)
-        self.parent.add_child(self.current)
+        self.current = Heading(parent, level, text)
+
+        if level == 1 and self.out.root is None:
+            self.out.root = self.current
+        else:
+            parent.add_child(self.current)
 
         print(f'Create: {str(self.current)}')
         return True
--- test.py
+++ test.py
@@ -12,6 +12,14 @@ Hello from Russia
 
 out = Parser().parse(text)
 assert out.title == 'Hello'
+assert out.source == '\n'
+
+assert out.root.text == 'Hello'
+assert out.root.source == 'Some text\n\n'
+
 assert out[0].text == 'Hello 2'
-assert out[0][0] == 'Hello 3'
+assert out[0].source == '\n'
+
+assert out[0][0].text == 'Hello 3'
+assert out[0][0].source == 'Hello from Russia\n'
 
